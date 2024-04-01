98db5d3d8d9f972e430bf5d1d51fbe40ffe31a24
Elliott Campbell <campbellelliott@me.com>
2024-04-01 13:28:09 +0100

    Initial Commit

---
+++ Ramen-Bowl/Ramen_BowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ramen_BowlApp.swift
+//  Ramen-Bowl
+//
+//  Created by Elliott Campbell on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Ramen_BowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
